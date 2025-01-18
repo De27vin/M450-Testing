85025f296acfb3825c1ac08675a16ab2b21eed5d
mikeezvz <mike.zogheib@outlook.com>
2025-01-18 15:10:07 +0100

    update: calculator.test.js for improved readability

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -2,10 +2,12 @@
 
 const calculator = require('../calculator');
 
+// test if 1+2 equals 3
 test('adds 1 to 2 equal 3', () => {
     expect(calculator.sum(1,2)).toBe(3)
 })
 
+// test if 3-2 equals 1 
 test('subtrakts 3 minus 2 equal 1', () => {
     expect(calculator.sub(3,2)).toBe(1)
 })
